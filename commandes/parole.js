e59bd4db12c2c9176052ed2d31ed4ddab0df9679
PAPA IGWE <papaigwe0@gmail.com>
2024-10-19 02:07:46 +0100

    Update parole.js

--- commandes/parole.js
+++ commandes/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------*Gojou-md*--------
+    let msg = `---------*Kamb-md*--------
 
 * *Artist :* ${lyrics.artist}
 
